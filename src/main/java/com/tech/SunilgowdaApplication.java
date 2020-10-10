139f17ed406b94a062fe0e4665e9805a8d4167dd
unknown <you@example.com>
2020-10-11 02:56:51 +0530

    initial

---
+++ src/main/java/com/tech/SunilgowdaApplication.java
@@ -0,0 +1,14 @@
+package com.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunilgowdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunilgowdaApplication.class, args);
+		System.out.println("-----------------------------------------Sunil-------------------------------------");
+	}
+
+}
